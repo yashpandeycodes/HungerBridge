85df8f4481844cfd0f0c811ce8c74ce7cdc25227
Yash Pandey <yashpandey040709@gmail.com>
2026-06-29 00:56:18 +0530

    Fixed

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,7 +2,7 @@ import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export async function middleware(req: NextRequest) {
+export async function proxy(req: NextRequest) {
   const { pathname } = req.nextUrl;
 
   if (
